29f1c28c9862b21ee4f6abd04c4be35afc35c1f6
Jonathan Besse <jonathan.besse@etu.umontpellier.fr>
2022-03-16 11:41:59 +0100

    Arbre syntaxique en programmation

--- src/main/scala/Rational.scala
+++ src/main/scala/Rational.scala
@@ -28,20 +28,46 @@ class RationalIsFractional(x: Rational, y: Rational) extends Fractional[Rational
     new Rational(x, 1)
 
   override def parseString(str: String): Option[Rational] =
-    var split:Array[String] = new Array[String](2)
-    split = str.split("/")
-    new Rational(split(0), split(1))
+    val split:Array[String] = str.split("/")
+    if (split(1) == "") None else Some(new Rational(split(0).toInt, split(1).toInt))
 
-  override def toInt(x: Rational): Int = ???
 
-  override def toLong(x: Rational): Long = ???
+  override def toInt(x: Rational): Int =
+    (x.a/x.b)
 
-  override def toFloat(x: Rational): Float = ???
+  override def toLong(x: Rational): Long =
+    x.a/x.b
 
-  override def toDouble(x: Rational): Double = ???
+  override def toFloat(x: Rational): Float =
+    x.a/x.b
 
-  override def compare(x: Rational, y: Rational): Int = ???
+  override def toDouble(x: Rational): Double =
+    x.a/x.b
+
+  override def compare(x: Rational, y: Rational): Int =
+    val a = x.a*y.b
+    val b = y.a*x.b
+    if a>b then 1 else if a == b then 0 else -1
+
 }
+
+enum SymbolicFunction:
+  case Constant(v: RationalIsFractional)
+  case Neg(s: SymbolicFunction)
+  case Add(s1: SymbolicFunction, s2: SymbolicFunction)
+  case Sub(s1: SymbolicFunction, s2: SymbolicFunction)
+  case Mult(s1: SymbolicFunction, s2: SymbolicFunction)
+
+
+  def eval(s: SymbolicFunction): RationalIsFractional = s match
+    case ArithExpr.Constant(x) =>
+    case ArithExpr.Neg(x) => negate(x)
+    case ArithExpr.Add(x, y) => eval(x)+eval(y)
+    case ArithExpr.Sub(x, y) => eval(x)-eval(y)
+    case ArithExpr.Mult(x, y) => eval(x)*eval(y)
+
+
+
 
 
 
